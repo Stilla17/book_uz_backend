b6f6f6dcc33da10abfa8a037a07f2eef3e7130b9
Stilla17 <sunnatilla95@gmail.com>
2026-06-17 17:51:25 +0500

    banner togirlandi

--- src/models/Banner.js
+++ src/models/Banner.js
@@ -6,14 +6,10 @@ const BannerSchema = new mongoose.Schema(
     imageUrl: { type: String, required: true },
     link: { type: String, default: "" },
     order: { type: Number, default: 0 },
-    // isActive: { type: Boolean, default: true },
-    // views: { type: Number, default: 0 },
-    // clicks: { type: Number, default: 0 },
   },
   { timestamps: true },
 );
 
 BannerSchema.index({ order: 1 });
-BannerSchema.index({ isActive: 1 });
 
 module.exports = mongoose.model("Banner", BannerSchema);
